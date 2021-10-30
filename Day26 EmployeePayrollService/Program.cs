81b26b4891af0313dd0912f3ab5eea972c298347
RAJARATHINAM-S <rajarathinams.mech@gmail.com>
2021-10-30 09:42:07 +0530

    Add project files.

---
+++ Day26 EmployeePayrollService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day26_EmployeePayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,38 @@
+﻿CREATE TABLE employee_payrollservice
+
+(
+	[Id] INT PRIMARY KEY Identity,
+	name varchar (50),
+	phone varchar(15),
+	address varchar (100),
+	department varchar (20),
+	gender varchar(1),
+	basicpay money,
+	deductions money,
+	taxablepay money,
+	tax money,
+	netpay money,
+	startdate date,
+
+);
+
+create procedure [dbo].[SpAddEmployeeDetails]
+@EmployeeName varchar(50),
+@PhoneNumber varchar(15),
+@Address varchar(100),
+@Department varchar(20),
+@Gender varchar(1),
+@BasicPay money,
+@Deductions money,
+@TaxablePay money,
+@Tax money,
+@NetPay money,
+@StartDate date
+as
+
+insert into employee_payroll1 values (@EmployeeName, @PhoneNumber, @Address, @Department, @Gender, @BasicPay, @Deductions, @TaxablePay, @Tax, @NetPay, @StartDate)
+
+go
+
+ select * from employee_payroll ;
+
